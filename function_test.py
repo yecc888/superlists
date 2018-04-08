437e24e2451cc8022f7836fe2b745f1f592bf600
yecc <18500398806@163.com>
2018-04-09 00:54:48 +0800

    add unittest

--- function_test.py
+++ function_test.py
@@ -1,6 +1,23 @@
 from selenium import webdriver
-driver = webdriver.Firefox()
-driver.get("http://127.0.0.1:8000/")
-assert "Django" in driver.title
-t = driver.title
-print(t)
+import unittest
+
+class Testfirst(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = webdriver.Firefox()
+        #driver.get("http://127.0.0.1:8000/")
+
+    def tearDown(self):
+        self.driver.quit()
+
+    def test_can_list(self):
+        self.driver.get("http://127.0.0.1:8000/")
+        self.assertIn('TO-DO', self.driver.title)
+        self.fail('test over')
+        t = driver.title
+        print(t)
+
+if __name__ == '__main__':
+    unittest.main()
+
+
