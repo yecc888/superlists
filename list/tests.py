abecfa383020b8328087565021f0894a468fa410
yecc <18500398806@163.com>
2018-04-11 22:54:58 +0800

    modified tests

--- list/tests.py
+++ list/tests.py
@@ -3,5 +3,5 @@ from django.test import TestCase
 # Create your tests here
 class SmokeTest(TestCase):
 
-    def bad_method(self):
+    def test_bad_method(self):
         self.assertEqual(1 + 1, 3)
