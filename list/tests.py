2a1a905bf087ee711eefde0299dadf28122e9f38
yecc <18500398806@163.com>
2018-04-11 23:26:16 +0800

    first do urls

--- list/tests.py
+++ list/tests.py
@@ -1,7 +1,11 @@
 from django.test import TestCase
+from django.core.urlresolvers import resolve
+from list.views import home_page
+
 
 # Create your tests here
 class SmokeTest(TestCase):
 
-    def test_bad_method(self):
-        self.assertEqual(1 + 1, 3)
+    def test_root_rul(self):
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
