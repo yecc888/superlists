a5ea74aa112bb26fa02e97c0f280c6f7f07324d2
yecc <18500398806@163.com>
2018-11-05 20:49:31 +0800

    add text.txt

--- list/tests.py
+++ list/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from django.core.urlresolvers import resolve
+from django.http import HttpRequest
 from list.views import home_page
 
 
@@ -9,3 +10,10 @@ class SmokeTest(TestCase):
     def test_root_rul(self):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
+
+    def test_home_page_some(self):
+        request = HttpRequest()
+        respones = home_page(request)
+        self.assertTrue(respones.content.startswith(b'<html>'))
+        self.assertIn(b'<title>TO-DO list</title>', respones.content)
+        self.assertTrue(respones.content.endswith(b'</html>'))
